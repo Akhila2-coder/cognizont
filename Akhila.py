b319b685610da05f24b848ef06df44b01b29961a
Akhila2-coder <akhilabairi455@gmail.com>
2026-06-11 15:56:25 +0530

    Add files via upload

---
+++ Akhila.py
@@ -0,0 +1,61 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+==== RESTART: C:/Users/pc/AppData/Local/Programs/Python/Python314/akhila.py ====
+Akhila
+
+==== RESTART: C:/Users/pc/AppData/Local/Programs/Python/Python314/akhila.py ====
+Akhila
+Awdc
+
+==== RESTART: C:/Users/pc/AppData/Local/Programs/Python/Python314/akhila.py ====
+Akhila
+Awdc
+200
+
+==== RESTART: C:/Users/pc/AppData/Local/Programs/Python/Python314/akhila.py ====
+Akhila
+Awdc
+200
+1000
+sum = 30
+
+==== RESTART: C:/Users/pc/AppData/Local/Programs/Python/Python314/akhila.py ====
+Akhila
+Awdc
+200
+1000
+sum = 30
+sum = 30
+>>> 
+==== RESTART: C:/Users/pc/AppData/Local/Programs/Python/Python314/akhila.py ====
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+>>> 
+==== RESTART: C:/Users/pc/AppData/Local/Programs/Python/Python314/akhila.py ====
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
